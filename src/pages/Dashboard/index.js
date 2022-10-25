74a6eb3ea2058596d84e28f803c71758c16512b3
Renner Borges <rennerferreira23@gmail.com>
2022-10-25 08:02:21 -0300

    feat: search input

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,21 +1,39 @@
-import { Text, Collapse, Col, Row } from '@nextui-org/react';
+import { Text, Collapse, Col, Row, Input } from '@nextui-org/react';
 import Nav from './components/NavBar/index.js';
 import styles from './styles.module.css';
 import dynamic from 'next/dynamic';
+import { useState } from 'react';
+import { MdSearch } from 'react-icons/md';
 
 const Editor = dynamic(() => import('../../components/Editor/index.js'), {
   ssr: false,
 });
 
 export default function Dashboard() {
+  const [search, setSearch] = useState('');
+
   return (
     <section>
       <Nav />
       <section className={styles.container}>
-        <Row>
+        <Row justify="space-between" align="center">
           <Text b size={25} css={{ color: '$accents9' }}>
             Seus objetos
           </Text>
+          <Row align="center" fluid={false}>
+            <Input
+              value={search}
+              onChange={(event) => {
+                const { value } = event.target;
+                setSearch(value);
+              }}
+              clearable
+              placeholder="Ex.: Cadeira"
+              color="default"
+              start
+            />
+            <MdSearch className={styles.iconSearch} size={24} />
+          </Row>
         </Row>
 
         <Collapse.Group>
@@ -43,7 +61,16 @@ export default function Dashboard() {
               Objeto:
             </Text>
             <Editor />
-            <Text b size={20} css={{ tt: 'capitalize', color: '$accents9' }}>
+            <Text
+              b
+              size={20}
+              css={{
+                display: 'block',
+                marginTop: '15px',
+                tt: 'capitalize',
+                color: '$accents9',
+              }}
+            >
               Model:
             </Text>
             <Editor />
